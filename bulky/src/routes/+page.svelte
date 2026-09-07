de701a5032b79be1779b1da2b5ae176bfbb6f96e
Selena <ssatnguyenn@gmail.com>
2026-09-06 23:04:41 -0400

    fix website

--- bulky/src/routes/+page.svelte
+++ bulky/src/routes/+page.svelte
@@ -244,26 +244,26 @@
         <h2 class="grid justify-center items-center text-xl font-bold pt-10">Frequently Asked Questions</h2>
         <div class="grid grid-cols-2 p-4 m-4 gap-2">
             <Accordionitem title="What is Bulky?">
-                <p class="text-xl">Bulky is a project to help you organize your hardware components</p>
+                <p class="text-md">Bulky is a project to help you organize your hardware components</p>
             </Accordionitem>
             <Accordionitem title="What is Hack Club?">
-                <p class="text-xl">Hack Club is a 501(c)(3) nonprofit and network of 100k+ technical high schoolers. We believe you learn best by building, so we’re creating community and providing grants so you can make awesome projects.</p>
+                <p class="text-md">Hack Club is a 501(c)(3) nonprofit and network of 100k+ technical high schoolers. We believe you learn best by building, so we’re creating community and providing grants so you can make awesome projects.</p>
             </Accordionitem>
             <Accordionitem title="Who can particpate?">
-                <p class="text-xl">Ages 13-18 inclusive can participate in Bulky.</p>
+                <p class="text-md">Ages 13-18 inclusive can participate in Bulky.</p>
             </Accordionitem>
             <Accordionitem title="When is the deadline?">
-                <p class="text-xl">The deadline for both design and build is October 2nd, 11:59 PM EST.</p>
+                <p class="text-md">The deadline for both design and build is October 2nd, 11:59 PM EST.</p>
             </Accordionitem>
             <Accordionitem title="How do I track my hours?">
-                <p class="text-xl">For both design and build hours, you can use Lapse!</p>
+                <p class="text-md">For both design and build hours, you can use Lapse!</p>
             </Accordionitem>
             <Accordionitem title="What storage organizer can I get?">
-                <p class="text-xl">It's dependent on your hours tracked! I recommend getting a <a href="https://www.amazon.com/Akro-Mils-10144-20-Inch-16-Inch-Hardware/dp/B003P2UOCO/ref=sr_1_7?crid=P9MGASZLXFVX&dib=eyJ2IjoiMSJ9.wGfIJ7Npm9R-npky_ZAtdNIkmx91C7IgUo5EvcUWZR_HDUxMliir2ZoRGfxpuor9unTLD8tetWfSgrr1oFpoQ-qfP3Ms5Xq-3BBMQwAerAjchiX3PDTRAlGq1Wf3Tsw8TB8PsUrMAPpMkpq33DKFewV8WYz3gBG9RUGsEUFh--YI8Hw2USEJHodAISTi9EUiKFOwH_C899RuliwZfW3TyfGA3RwbwBwcGf6ZgwMoBbQHGlqTcPg7gV47_4C0nj3al2S2Q0pfOuzT-6ZnnTaLyfkCJS9xKcS63Ij49if_R1w.0sG6UZ7DDvrEyJ8H4dcqe3SsE9XV6adWctBOHqAPkm8&dib_tag=se&keywords=bin%2Borganizer&qid=1788286872&sprefix=bin%2Borganizer%2Caps%2C115&sr=8-7&th=1" class="hover:underline">plastic bin cabinet</a> or the <a href="https://www.amazon.com/Jack-Boss-Transparent-34-Compartments-Accessories/dp/B0CZDJNV19/ref=sr_1_2?crid=3JLRE2JHIU0EZ&dib=eyJ2IjoiMSJ9.2j8a7mkV4onyHodjI_DLTxLRN6XCoq3RdDESGmHvSmo83X5kjKP8eaYOxo8pXFyULTkrziir7SSz1HGY0JMUoV1sthd1R9mfJsdqnongzy0Vae6OpdUpns42rsuRD-N2hzXB1MgEGKsUanSNwSzMFJO5LxPyhtHm1Tnd79FswabUch4vdUpuTNuYv1OOOo2RPVrDxdbu-HnZWHdPOs0ySjG1zR31j5WMMxzVC4sLmTXs8d_KCX20D5fSSOcNTE3r4sQhDOS9WcTVUtinyVIj5DvVRHl_PFCEuT7TXNmQB04.PMEqEX7KQ8mJsjJzYU0hSD7lGAUzzskGyMJ2MA4ydPs&dib_tag=se&keywords=small%2Bbin%2Borganizer%2Bportable&qid=1788287027&sprefix=small%2Bbin%2Borganizer%2Bportabl%2Caps%2C122&sr=8-2&th=1" class="hover:underline">portable tool box</a>.</p>
+                <p class="text-md">It's dependent on your hours tracked! I recommend getting a <a href="https://www.amazon.com/Akro-Mils-10144-20-Inch-16-Inch-Hardware/dp/B003P2UOCO/ref=sr_1_7?crid=P9MGASZLXFVX&dib=eyJ2IjoiMSJ9.wGfIJ7Npm9R-npky_ZAtdNIkmx91C7IgUo5EvcUWZR_HDUxMliir2ZoRGfxpuor9unTLD8tetWfSgrr1oFpoQ-qfP3Ms5Xq-3BBMQwAerAjchiX3PDTRAlGq1Wf3Tsw8TB8PsUrMAPpMkpq33DKFewV8WYz3gBG9RUGsEUFh--YI8Hw2USEJHodAISTi9EUiKFOwH_C899RuliwZfW3TyfGA3RwbwBwcGf6ZgwMoBbQHGlqTcPg7gV47_4C0nj3al2S2Q0pfOuzT-6ZnnTaLyfkCJS9xKcS63Ij49if_R1w.0sG6UZ7DDvrEyJ8H4dcqe3SsE9XV6adWctBOHqAPkm8&dib_tag=se&keywords=bin%2Borganizer&qid=1788286872&sprefix=bin%2Borganizer%2Caps%2C115&sr=8-7&th=1" class="hover:underline">plastic bin cabinet</a> or the <a href="https://www.amazon.com/Jack-Boss-Transparent-34-Compartments-Accessories/dp/B0CZDJNV19/ref=sr_1_2?crid=3JLRE2JHIU0EZ&dib=eyJ2IjoiMSJ9.2j8a7mkV4onyHodjI_DLTxLRN6XCoq3RdDESGmHvSmo83X5kjKP8eaYOxo8pXFyULTkrziir7SSz1HGY0JMUoV1sthd1R9mfJsdqnongzy0Vae6OpdUpns42rsuRD-N2hzXB1MgEGKsUanSNwSzMFJO5LxPyhtHm1Tnd79FswabUch4vdUpuTNuYv1OOOo2RPVrDxdbu-HnZWHdPOs0ySjG1zR31j5WMMxzVC4sLmTXs8d_KCX20D5fSSOcNTE3r4sQhDOS9WcTVUtinyVIj5DvVRHl_PFCEuT7TXNmQB04.PMEqEX7KQ8mJsjJzYU0hSD7lGAUzzskGyMJ2MA4ydPs&dib_tag=se&keywords=small%2Bbin%2Borganizer%2Bportable&qid=1788287027&sprefix=small%2Bbin%2Borganizer%2Bportabl%2Caps%2C122&sr=8-2&th=1" class="hover:underline">portable tool box</a>.</p>
             </Accordionitem>
             <Accordionitem title="I've designed the hardware, but I need a couple of components to finish it. Can I get a grant?">
-                <p class="text-xl">You should have most of the parts for your project, but we can give you a grant to up to 25$ to get the remaining components.</p>
-            </Accordionitem>
+                <p class="text-md">You should have most of the parts for your project, but we can give you a grant to up to 25$ to get the remaining components.</p>
+            </Accordionitem>    
         </div> 
     </div>
     <footer class="flex flex-col justify-center items-center w-full h-full py-6 bg-gray-800 text-white">
